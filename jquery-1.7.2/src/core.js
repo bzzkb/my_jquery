430af43891ea64d69228e0010d1216ad9102739d
xuanxiao <xuanxiao2013@sina.cn>
2013-03-02 17:59:28 +0800

    complete core.js

--- jquery-1.7.2/src/core.js
+++ jquery-1.7.2/src/core.js
@@ -175,11 +175,14 @@ jQuery.fn = jQuery.prototype = {
 
 					if ( ret ) {
 						//处理$('<div/>')
+						
 						if ( jQuery.isPlainObject( context ) ) {
+							//处理$('<div/>', {'width': 10, ...})
 							selector = [ document.createElement( ret[1] ) ];
 							jQuery.fn.attr.call( selector, context, true );
 
 						} else {
+							//处理$('<div/>')
 							selector = [ doc.createElement( ret[1] ) ];
 						}
 
@@ -242,25 +245,36 @@ jQuery.fn = jQuery.prototype = {
 	},
 
 	// Start with an empty selector
+	// 选择器,就是括号中的字符串   $('selector')
+	// eg: 1. $("#qunit-fixture").filter("div") 的selector = #qunit-fixture.filter(div)
+	// 2.  $("#qunit-fixture")  的selector = #qunit-fixture
 	selector: "",
 
 	// The current version of jQuery being used
 	jquery: "@VERSION",
-
+	
+	//大家发现这个length和size说的一回事，为啥又分为两个，
+	//我的理解是：length是从数组的完备性来考虑的，而size则是从对象的角度来考虑
+	//jquery是个对象，也可以转化为数组
+	
 	// The default length of a jQuery object is 0
+	// jquery对象是个伪数组,返回数组的长度，这个
 	length: 0,
 
 	// The number of elements contained in the matched element set
+	// 当前jquery对象的大小,既这个类数组的长度
 	size: function() {
 		return this.length;
 	},
-
+	//返回一个数组
 	toArray: function() {
 		return slice.call( this, 0 );
 	},
 
 	// Get the Nth element in the matched element set OR
 	// Get the whole matched element set as a clean array
+	// 想数组一样获得jquery对象中元素, 这个num可以使正数，也可以是负数, 当让也可以为空，则以数组的方式jquery中的所有元素
+	// 注意返回的可不是jquery对象
 	get: function( num ) {
 		return num == null ?
 
@@ -273,11 +287,15 @@ jQuery.fn = jQuery.prototype = {
 
 	// Take an array of elements and push it onto the stack
 	// (returning the new matched element set)
+	// 就是说把elems包装成jquery对象返回
 	pushStack: function( elems, name, selector ) {
 		// Build a new jQuery matched element set
+		// 新建一个jquery对象
 		var ret = this.constructor();
-
+		
+		// 添加elems到jquery对象中
 		if ( jQuery.isArray( elems ) ) {
+			//数组
 			push.apply( ret, elems );
 
 		} else {
@@ -315,25 +333,26 @@ jQuery.fn = jQuery.prototype = {
 
 		return this;
 	},
-
+	//返回jquery集合中的第i个元素，这里返回的可视jquery对象
+	//这个i可以是正数，也可以是负数
 	eq: function( i ) {
+		//这一行的作用是啥啊？
 		i = +i;
 		return i === -1 ?
 			this.slice( i ) :
 			this.slice( i, i + 1 );
 	},
-
+	//返回jquery集合中的第一个元素，这里返回的可视jquery对象
 	first: function() {
 		return this.eq( 0 );
 	},
-
+	//返回jquery集合中的最后一个元素，这里返回的可视jquery对象
 	last: function() {
 		return this.eq( -1 );
 	},
-
+	//数组切分
 	slice: function() {
-		return this.pushStack( slice.apply( this, arguments ),
-			"slice", slice.call(arguments).join(",") );
+		return this.pushStack( slice.apply( this, arguments ), "slice", slice.call(arguments).join(",") );
 	},
 
 	map: function( callback ) {
@@ -341,7 +360,8 @@ jQuery.fn = jQuery.prototype = {
 			return callback.call( elem, i, elem );
 		}));
 	},
-
+	// 返回节点链式操作前一次操作的节点,要是没有前一个节点，则返回空的jquery对象
+	// equal( "Yahoo", jQuery("#yahoo").parent().end().text(), "Check for end" );
 	end: function() {
 		return this.prevObject || this.constructor(null);
 	},
@@ -356,6 +376,24 @@ jQuery.fn = jQuery.prototype = {
 // Give the init function the jQuery prototype for later instantiation
 jQuery.fn.init.prototype = jQuery.fn;
 
+/**
+ * 
+ *   如果传入两个或多个对象，所有对象的属性会被添加到第一个对象target,相同的属性值会被后面的覆盖
+ *   
+ *	 如果只传入一个对象，则将对象的属性添加到jQuery对象中。
+ *
+ *	 用这种方式，我们可以为jQuery命名空间增加新的方法。可以用于编写jQuery插件。
+ *
+ *	 如果不想改变传入的对象，可以传入一个空对象：$.extend({}, object1, object2);
+ *
+ *	 默认合并操作是不迭代的，即便target的某个属性是对象或属性，也会被完全覆盖而不是合并
+ *	 第一个参数是true，则会迭代合并从object原型继承的属性会被拷贝
+ *
+ *	 undefined值不会被拷贝, 因为性能原因，JavaScript自带类型的属性不会合并
+ *
+ *	 jQuery.extend( target, object1, ... , objectN )
+ * 	 jQuery.extend( [ deep ], target, object1, ... , objectN )
+ */
 jQuery.extend = jQuery.fn.extend = function() {
 	var options, name, src, copy, copyIsArray, clone,
 		target = arguments[0] || {},
@@ -364,6 +402,7 @@ jQuery.extend = jQuery.fn.extend = function() {
 		deep = false;
 
 	// Handle a deep copy situation
+	//处理深度拷贝
 	if ( typeof target === "boolean" ) {
 		deep = target;
 		target = arguments[1] || {};
@@ -372,33 +411,42 @@ jQuery.extend = jQuery.fn.extend = function() {
 	}
 
 	// Handle case when target is a string or something (possible in deep copy)
+	// 处理第一个参数，如果第一个参数不是个对象，也不是函数， 就初始化为一个空的对象
 	if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
 		target = {};
 	}
 
 	// extend jQuery itself if only one argument is passed
+	// 如果就一个参数，则扩展jquery
 	if ( length === i ) {
 		target = this;
 		--i;
 	}
-
+	//开始扩展
 	for ( ; i < length; i++ ) {
 		// Only deal with non-null/undefined values
+		// 这里只处理不为null和undefined的值
 		if ( (options = arguments[ i ]) != null ) {
 			// Extend the base object
+			//扩展基本对象
 			for ( name in options ) {
+				//原来的
 				src = target[ name ];
+				//要扩展的
 				copy = options[ name ];
 
 				// Prevent never-ending loop
+				// 翻译过来时防止死循环，不过还是没有看明白
 				if ( target === copy ) {
 					continue;
 				}
 
 				// Recurse if we're merging plain objects or arrays
+				//递归，如果是纯对象或数组则合并
 				if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
 					if ( copyIsArray ) {
 						copyIsArray = false;
+						// clone = (src && jQuery.isArray(src)) ? src : [];
 						clone = src && jQuery.isArray(src) ? src : [];
 
 					} else {
@@ -537,21 +585,27 @@ jQuery.extend({
 	isArray: Array.isArray || function( obj ) {
 		return jQuery.type(obj) === "array";
 	},
-
+	//确定参数是否为一个窗口（window对象）。
+	//这是在jQuery中用在一些地方的，以确定我们的操作是否为一个浏览器窗口操作。（如当前窗口或一个iframe）。
 	isWindow: function( obj ) {
 		return obj != null && obj == obj.window;
 	},
 
 	isNumeric: function( obj ) {
+		//http://www.w3school.com.cn/js/jsref_obj_global.asp
+		//这两个是js 的全局函数
+		//这里的逻辑是  是数字并且不能是无穷大
 		return !isNaN( parseFloat(obj) ) && isFinite( obj );
 	},
-
+	// 判断类型
 	type: function( obj ) {
+		//equal( jQuery.type(null), "null", "null" );
+		//equal( jQuery.type(undefined), "undefined", "undefined" );
 		return obj == null ?
 			String( obj ) :
 			class2type[ toString.call(obj) ] || "object";
 	},
-
+	//测试对象是否是纯粹的对象
 	isPlainObject: function( obj ) {
 		// Must be an Object.
 		// Because of IE, we also have to check the presence of the constructor property.
@@ -591,7 +645,7 @@ jQuery.extend({
 	error: function( msg ) {
 		throw new Error( msg );
 	},
-
+	//转化为json
 	parseJSON: function( data ) {
 		if ( typeof data !== "string" || !data ) {
 			return null;
@@ -601,12 +655,14 @@ jQuery.extend({
 		data = jQuery.trim( data );
 
 		// Attempt to parse using the native JSON parser first
+		// 尝试浏览器原生的方法
 		if ( window.JSON && window.JSON.parse ) {
 			return window.JSON.parse( data );
 		}
 
 		// Make sure the incoming data is actual JSON
 		// Logic borrowed from http://json.org/json2.js
+		//用json2解析
 		if ( rvalidchars.test( data.replace( rvalidescape, "@" )
 			.replace( rvalidtokens, "]" )
 			.replace( rvalidbraces, "")) ) {
@@ -618,6 +674,7 @@ jQuery.extend({
 	},
 
 	// Cross-browser xml parsing
+	//解析xml
 	parseXML: function( data ) {
 		if ( typeof data !== "string" || !data ) {
 			return null;
@@ -642,7 +699,7 @@ jQuery.extend({
 	},
 
 	noop: function() {},
-
+	//在全局上下文中计算一个脚本
 	// Evaluates a script in a global context
 	// Workarounds based on findings by Jim Driscoll
 	// http://weblogs.java.net/blog/driscoll/archive/2009/09/08/eval-javascript-global-context
@@ -659,6 +716,7 @@ jQuery.extend({
 
 	// Convert dashed to camelCase; used by the css and data modules
 	// Microsoft forgot to hump their vendor prefix (#9572)
+	// 把- 转化为驼峰标识
 	camelCase: function( string ) {
 		return string.replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
 	},
@@ -668,12 +726,15 @@ jQuery.extend({
 	},
 
 	// args is for internal usage only
+	// args 这个参数只是给内部用
 	each: function( object, callback, args ) {
 		var name, i = 0,
+			//看好了，这里的object是个数组，不是个对象，对象没有length属性
 			length = object.length,
 			isObj = length === undefined || jQuery.isFunction( object );
 
 		if ( args ) {
+			//内部使用
 			if ( isObj ) {
 				for ( name in object ) {
 					if ( callback.apply( object[ name ], args ) === false ) {
@@ -690,13 +751,16 @@ jQuery.extend({
 
 		// A special, fast, case for the most common use of each
 		} else {
+			//对外公开
 			if ( isObj ) {
+				//对象
 				for ( name in object ) {
 					if ( callback.call( object[ name ], name, object[ name ] ) === false ) {
 						break;
 					}
 				}
 			} else {
+				//数组
 				for ( ; i < length; ) {
 					if ( callback.call( object[ i ], i, object[ i++ ] ) === false ) {
 						break;
@@ -724,6 +788,9 @@ jQuery.extend({
 		},
 
 	// results is for internal usage only
+		// results 这个参数只是内部使用
+		//返回一个数组
+		// 注意这个测试用例deepEqual( jQuery.makeArray({length: "0"}), [], "Make sure object is coerced properly.");
 	makeArray: function( array, results ) {
 		var ret = results || [];
 
@@ -741,7 +808,15 @@ jQuery.extend({
 
 		return ret;
 	},
-
+	//判断一个元素是否在一个数组中
+	//返回 -1 表示 不在这个数组中， 如果从在则返回索引
+	/**
+	 * elem 元素
+	 * array 数组
+	 * i 开始查找位置, 如果不传，则为0
+	 * 
+	 * 注意：elem 为 false、null、undefined 时 返回-1，不报错
+	 */
 	inArray: function( elem, array, i ) {
 		var len;
 
@@ -751,6 +826,10 @@ jQuery.extend({
 			}
 
 			len = array.length;
+			//这一行写的可真是个绕的 
+			// i = (i ? (i < 0 ? Math.max( 0, len + i ) : i) : 0);
+			//说的是这个意思：如果i为定义，则返回0，如果i大于0则返回i。小于0则返回Math.max( 0, len + i )
+			// 总之i是个大于0 的数
 			i = i ? i < 0 ? Math.max( 0, len + i ) : i : 0;
 
 			for ( ; i < len; i++ ) {
@@ -763,11 +842,16 @@ jQuery.extend({
 
 		return -1;
 	},
-
+	//second 合并到  first 中
 	merge: function( first, second ) {
 		var i = first.length,
 			j = 0;
-
+		//有个疑问：这个if判断做啥用的啊
+		//下面的这个测试用例无法通过
+		//deepEqual( jQuery.merge([], [null, undefined]), [null, undefined], "Second array including null and undefined values");
+		//还是有点不明白, 下面这个测试用例就算有if也是无法通过
+		// jQuery.merge({a: 1, b: undefined},{}), {a: 1, b: undefined})
+		//综上：看来用这个方法的注意是否包含undefined
 		if ( typeof second.length === "number" ) {
 			for ( var l = second.length; j < l; j++ ) {
 				first[ i++ ] = second[ j ];
@@ -801,18 +885,42 @@ jQuery.extend({
 	},
 
 	// arg is for internal usage only
+	// arg 这个参数是给内部使用的
 	map: function( elems, callback, arg ) {
 		var value, key, ret = [],
 			i = 0,
 			length = elems.length,
 			// jquery objects are treated as arrays
+			//这个判断真是绕的
+			/**
+			 * 	isArray = 
+			 * 		是jquery的实例
+			 * 		elems instanceof jQuery || 
+			 * 		是个数组，wokao，数组判断有这么复杂
+			 * 		要是个数组，的满足下面的条件：1. length是个数字, 2. (length大于0 并且 第一个元素 和最后一个元素不为空) 或 length === 0 或者是个js数组
+			 * 		(
+			 * 			length !== undefined && 
+			 * 			typeof length === "number" && 
+			 * 			( 
+			 * 				( length > 0 && elems[ 0 ] && elems[ length -1 ] ) || length === 0 || jQuery.isArray( elems ) 
+			 *			)
+			 *  	);
+			 */
 			isArray = elems instanceof jQuery || length !== undefined && typeof length === "number" && ( ( length > 0 && elems[ 0 ] && elems[ length -1 ] ) || length === 0 || jQuery.isArray( elems ) ) ;
-
 		// Go through the array, translating each of the items to their
 		if ( isArray ) {
+			//数组
 			for ( ; i < length; i++ ) {
 				value = callback( elems[ i ], i, arg );
-
+				
+				//下面这三行诗是啥意思啊
+				// 意思就是说如果map有返回值，如果返回值不为空，则把返回值记录在一个数组中
+				/**
+				 * var keys = jQuery.map( {a:'', c: 'a', b : null}, function( v, k ){
+				 *		return v;
+				 * });
+				 * output:['', 'a']
+				 */
 				if ( value != null ) {
 					ret[ ret.length ] = value;
 				}
@@ -820,6 +928,7 @@ jQuery.extend({
 
 		// Go through every key on the object,
 		} else {
+			//对象
 			for ( key in elems ) {
 				value = callback( elems[ key ], key, arg );
 
@@ -836,8 +945,8 @@ jQuery.extend({
 	// A global GUID counter for objects
 	guid: 1,
 
-	// Bind a function to a context, optionally partially applying any
-	// arguments.
+	// Bind a function to a context, optionally partially applying any arguments.
+	// 给一个上下文绑定一个函数
 	proxy: function( fn, context ) {
 		if ( typeof context === "string" ) {
 			var tmp = fn[ context ];
@@ -851,18 +960,21 @@ jQuery.extend({
 			return undefined;
 		}
 
-		// Simulated bind
+		// Simulated bind 模拟绑定
 		var args = slice.call( arguments, 2 ),
 			proxy = function() {
 				return fn.apply( context, args.concat( slice.call( arguments ) ) );
 			};
 
 		// Set the guid of unique handler to the same of original handler, so it can be removed
+			//给一个唯一的guid和原来处理函数的相同
+			//为啥要给guid?
 		proxy.guid = fn.guid = fn.guid || proxy.guid || jQuery.guid++;
 
 		return proxy;
 	},
-
+	
+	//多种方法来获取和设置值的集合
 	// Mutifunctional method to get and set values to a collection
 	// The value/s can optionally be executed if it's a function
 	access: function( elems, fn, key, value, chainable, emptyGet, pass ) {
